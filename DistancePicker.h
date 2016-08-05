fbf255617e34932af5b420806806b2e738f7dd49
Quentin Mathé <quentin.mathe@gmail.com>
2016-08-05 18:21:09 +0200

    Reorganized into a framework, plus a new app example

---
+++ DistancePicker.h
@@ -0,0 +1,19 @@
+//
+//  DistancePicker.h
+//  DistancePicker
+//
+//  Created by Quentin Mathé on 05/08/2016.
+//  Copyright © 2016 Quentin Mathé. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DistancePicker.
+FOUNDATION_EXPORT double DistancePickerVersionNumber;
+
+//! Project version string for DistancePicker.
+FOUNDATION_EXPORT const unsigned char DistancePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DistancePicker/PublicHeader.h>
+
+#import <DistancePicker/UIGestureRecognizer+MissingPublicAPI.h>
--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -1,25 +1,111 @@
-//
-//  ViewController.swift
-//  DistancePicker
-//
-//  Created by Quentin Mathé on 03/08/2016.
-//  Copyright © 2016 Quentin Mathé. All rights reserved.
-//
+/**
+	Copyright (C) 2016 Quentin Mathe
+ 
+	Date:  November 2016
+	License:  MIT
+ */
 
 import UIKit
+import MapKit
+import DistancePicker
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+	@IBOutlet var distancePicker: DistancePicker!
+	@IBOutlet var mapView: MKMapView!
+	var searchRadiusOverlay: MKOverlay?
+	var searchRadiusActive: Bool {
+		return distancePicker.selectedValue != DBL_MAX && isValidAuthorizationStatus(authorizationStatus)
+	}
+	var authorizationStatus = CLAuthorizationStatus.NotDetermined
+	var locationManager = CLLocationManager()
+	
+	// MARK: - Configuration
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		// Every time the user manipulates the distance picker, an action is 
+		// sent when the pan animation stops. We use this opportunity to update
+		// the map rect and search radius to match the selected distance.
+		distancePicker.target = self
+		distancePicker.action = #selector(updateUI)
+
+		mapView.delegate = self
+		mapView.hidden = true
+
+		locationManager.delegate = self
+		locationManager.requestWhenInUseAuthorization()
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func isValidAuthorizationStatus(status: CLAuthorizationStatus) -> Bool {
+		// For iOS 7, AuthorizedAlways corresponds to Authorized
+		return status == CLAuthorizationStatus.AuthorizedWhenInUse || status == CLAuthorizationStatus.AuthorizedAlways
 	}
 
+	func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+		authorizationStatus = status
 
+		if isValidAuthorizationStatus(status) {
+			mapView.showsUserLocation = true
+		}
+		updateUI()
+	}
+	
+	// MARK: - Updating UI
+	
+	func updateUI() {
+		updateSearchRadiusOverlay()
+		updateVisibleMapRect()
+	}
+	
+	func updateSearchRadiusOverlay() {
+		if let overlay = searchRadiusOverlay {
+			mapView.removeOverlay(overlay)
+			searchRadiusOverlay = nil
+		}
+
+		if searchRadiusActive {
+			searchRadiusOverlay = MKCircle(centerCoordinate: mapView.userLocation.coordinate,
+			                                         radius: distancePicker.selectedValue)
+			mapView.addOverlay(searchRadiusOverlay!)
+		}
+	}
+	
+	func updateVisibleMapRect() {
+		if searchRadiusActive {
+			let overlayInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+			let mapRect = mapView.mapRectThatFits(searchRadiusOverlay!.boundingMapRect, edgePadding: overlayInset)
+
+			mapView.setVisibleMapRect(mapRect, animated: false)
+		}
+		else if isValidAuthorizationStatus(authorizationStatus) {
+			mapView.setCenterCoordinate(mapView.userLocation.coordinate, animated: true)
+		}
+		// On launch, hide the map until we know the user location, otherwise 
+		// the map is briefly centered on another location.
+		mapView.hidden = false
+	}
+	
+	// MARK: - Map View Delegate
+	
+	func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+		updateUI()
+	}
+	
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let circle = MKCircleRenderer(overlay: overlay)
+
+            circle.strokeColor = UIColor.redColor()
+            circle.fillColor = UIColor.redColor().colorWithAlphaComponent(0.1)
+            circle.lineWidth = 1
+
+            return circle
+        }
+		else {
+			fatalError("Unexpected overlay type")
+		}
+    }
 }
 
